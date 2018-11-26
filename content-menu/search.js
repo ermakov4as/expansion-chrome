595b8fb076e3bca4e94971f311d5bbfe6daa35d0
ermakov4as <ermakov4as@gmail.com>
2018-11-27 02:04:25 +0300

    v0.0.0.12

--- content-menu/search.js
+++ content-menu/search.js
@@ -60,10 +60,28 @@ function addFromMenu(text) {
     xhr.onreadystatechange = function() {
         if (xhr.readyState != 4) return;
         if (xhr.status != 200) {
-            chrome.runtime.sendMessage({ type: 'error', text: xhr.status }, function(response) {});
+            createNotificationSearch({ type: 'error', text: xhr.status });
             console.log('Error! ' + xhr.status + ': ' + xhr.statusText);
         } else {
-            chrome.runtime.sendMessage({ type: 'success', text: text }, function(response) {});
+            createNotificationSearch({ type: 'success', text: text });
         };
     };
+};
+
+function createNotificationSearch(data) {
+    let title = "";
+    let msg = "";
+    if (data.type === 'success') {
+        title = "Добавлено";
+        msg = `Выделенный текст "${data.text}" добавлен в Тренажёр`;
+    } else if (data.type === 'error') {
+        title = "Ошибка!";
+        msg = `Ошибка ${data.text} при добавлении выделенного текста в Тренажёр`;
+    };
+    let timestamp = new Date().getTime();
+    let id = 'notificationID' + timestamp;
+    let opt = { type: "basic", title: title, message: msg, iconUrl: "./images/icon128.png" };
+    chrome.notifications.create(id, opt, function() {
+        setTimeout(function() { chrome.notifications.clear(id) }, 4000);
+    });
 };
